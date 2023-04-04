301a2e3c62fd824b43837965fce44e74208bafbf
GribanovDS <74065724+GribanovDS@users.noreply.github.com>
2023-04-04 17:25:26 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,17 @@ import numpy as np
 chat_id = 1219503064 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    time = 62
+    distances = x.copy()
+    distances /= ((time**2)/2)
     added = 10000
-    x+=added
+    distances+=added
     alpha = 1 - p
-    n = len(x)
-    min = x.min()
+    n = len(distances)
+    min = distances.min()
     zquant1 = expon.ppf(alpha/2)
     zquant2 = expon.ppf(1-alpha/2)
     left = zquant1 / (n*min)
     right = zquant2 / (n*min)
-    mean = x.mean()
+    mean = distances.mean()
     return (mean - left - added, mean + right - added)
