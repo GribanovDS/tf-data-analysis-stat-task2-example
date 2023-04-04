04d2254a1e0e69a295d5ae92f1006f328762b9fb
GribanovDS <74065724+GribanovDS@users.noreply.github.com>
2023-04-04 23:22:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-from scipy.stats import expon
+from scipy.stats import chi2, expon
 import numpy as np
 
 chat_id = 1219503064 # Ваш chat ID, не меняйте название переменной
@@ -6,15 +6,16 @@ chat_id = 1219503064 # Ваш chat ID, не меняйте название пе
 def solution(p: float, x: np.array) -> tuple:
     time = 62
     distances = x.copy()
+    distances = 0.5 - distances
     distances /= ((time**2)/2)
-    added = 10000
-    distances+=added
     alpha = 1 - p
     n = len(distances)
-    min = distances.min()
-    zquant1 = expon.ppf(alpha/2)
-    zquant2 = expon.ppf(1-alpha/2)
-    left = zquant1 / (n*min)
-    right = zquant2 / (n*min)
-    mean = distances.mean()
-    return (mean - left - added, mean + right - added)
+    df = 2 * n
+    sum = 0
+    for elem in distances:
+        sum+=elem
+    chi2_value_1 = chi2.ppf(alpha / 2, df)
+    chi2_value_2 = chi2.ppf(1 - alpha / 2, df)
+    left = 2 * sum / chi2_value_1 
+    right = 2 * sum / chi2_value_2
+    return (left, right)
