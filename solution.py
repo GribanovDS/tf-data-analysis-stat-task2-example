72e4819c12dc1b9daa0b30e514fd4025bad32d3b
GribanovDS <74065724+GribanovDS@users.noreply.github.com>
2023-04-07 14:35:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,25 +1,16 @@
-from scipy.stats import expon
 import numpy as np
 
 chat_id = 1219503064 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    eps = 0.001
-    time = 62
-    distances = x.copy()
-    distances /= ((time**2)/2)
-    size = len(distances)
-    diff = 0.00025
-    if ((size == 10) & (abs(p-0.95)<eps)):
-        diff = 0.00045
-    elif((size == 100) & (abs(p-0.9)<eps)):
-        diff = 0.0001
-    elif((size == 100) & (abs(p-0.7)<eps)):
-        diff = 0.0001
-    elif((size == 1000) & (abs(p-0.9)<eps)):
-        diff = 0.00025
-    elif((size == 1000) & (abs(p-0.99)<eps)):
-        diff = 0.0008
-        print('here')
-    mean = distances.mean()
-    return (mean - diff, mean + diff)
+    from math import log
+    # Время в секундах, за которое машина преодолела путь
+    time = 62 
+    # Размер выборки
+    n = len(x) 
+    # Квантиль
+    z = - np.log(1-p)/n 
+    # Доверительный интервал
+    return ( 2*(-(-x).min() - 1/2)/(time**2), 
+             2*(z - (-x).min() - 1/2)/(time**2) ) 
+
